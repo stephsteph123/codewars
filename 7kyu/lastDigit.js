ae5f45a77d0162270eee5449ea65f3a438900302
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-05-01 15:25:00 -0400

    update lastDigit kata

--- 7kyu/lastDigit.js
+++ 7kyu/lastDigit.js
@@ -6,4 +6,28 @@
 // If D <= 0, return an empty list.
 
 // solution:
+function lastDigit(n, d) {
+  const newN = n.toString().length
+  newArr = [];
+  if (d <= 0){
+    return [];
+  } else if (d > newN) {
+    let char = n.toString().split('')
+    for(i = 0; i <newN; i++){
+      newArr.push(char[i])
+    }
+  } else {
+    let char = n.toString().split('')
+    for(i = 1; i <newN-1; i++){
+      newArr.push(char[i])
+    }
+  }
+  console.log(newArr)
+}
+
+console.log(lastDigit(1343,5));
+console.log(lastDigit(1234,0));
+console.log(lastDigit(123767,4));
+console.log(lastDigit(34625647867585,10));
+
 
