bf58c563931d6d5d2db036599b7539f134981a9d
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-05-03 13:03:43 -0400

    Update alphabetWar.js

--- 7kyu/alphabetWar.js
+++ 7kyu/alphabetWar.js
@@ -15,29 +15,36 @@ const rightSide = {
   z: 1,
 };
 
-// solution
-function alphabetWar(fight) {
-  const leftArr = [];
-  const rightArr = [];
-  const newArr = fight.split('')
-  for (let i of newArr){
-    if (leftSide[i]){
-      leftArr.push(leftSide[i])
-    } else if (rightSide[i]) {
-    rightArr.push(rightSide[i])
-  }
-}
-leftFinal = leftArr.reduce((a, b) => a + b, 0)
-rightFinal = rightArr.reduce((a, b) => a + b, 0)
+// solution loops
+// function alphabetWar(fight) {
+//   const leftArr = [];
+//   const rightArr = [];
+//   const newArr = fight.split('')
+//   for (let i of newArr){
+//     if (leftSide[i]){
+//       leftArr.push(leftSide[i])
+//     } else if (rightSide[i]) {
+//     rightArr.push(rightSide[i])
+//   }
+// }
+// leftFinal = leftArr.reduce((a, b) => a + b, 0)
+// rightFinal = rightArr.reduce((a, b) => a + b, 0)
 
-if(leftFinal > rightFinal) {
-  return "Left side wins!";
-} else if (rightFinal > leftFinal){
-  return "Right side wins!";
-} else {
-    return "Let's fight again!";
+// if(leftFinal > rightFinal) {
+//   return "Left side wins!";
+// } else if (rightFinal > leftFinal){
+//   return "Right side wins!";
+// } else {
+//     return "Let's fight again!";
+// }
+// };
+
+// solution refactor
+function alphabetWar(fight) {
+  let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+  let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+  return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
 }
-};
 
 alphabetWar("z");
 alphabetWar("zdqmwpbs");
