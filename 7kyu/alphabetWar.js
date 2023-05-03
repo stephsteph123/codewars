0517079d1851ec38839c91c6dc62f9bb2b3d25a2
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-05-03 13:01:55 -0400

    Create alphabetWar.js

---
+++ 7kyu/alphabetWar.js
@@ -0,0 +1,45 @@
+// problem:
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+const leftSide = {
+  w: 4,
+  p: 3,
+  b: 2,
+  s: 1,
+};
+
+const rightSide = {
+  m: 4,
+  q: 3,
+  d: 2,
+  z: 1,
+};
+
+// solution
+function alphabetWar(fight) {
+  const leftArr = [];
+  const rightArr = [];
+  const newArr = fight.split('')
+  for (let i of newArr){
+    if (leftSide[i]){
+      leftArr.push(leftSide[i])
+    } else if (rightSide[i]) {
+    rightArr.push(rightSide[i])
+  }
+}
+leftFinal = leftArr.reduce((a, b) => a + b, 0)
+rightFinal = rightArr.reduce((a, b) => a + b, 0)
+
+if(leftFinal > rightFinal) {
+  return "Left side wins!";
+} else if (rightFinal > leftFinal){
+  return "Right side wins!";
+} else {
+    return "Let's fight again!";
+}
+};
+
+alphabetWar("z");
+alphabetWar("zdqmwpbs");
+alphabetWar("zzzzs");
+alphabetWar("wwwwww");
