9886ff2dbd6840a4c40553abe587dc8e4c1feda1
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-05-11 11:12:16 -0400

    Create returnStr.js

---
+++ 8kyu/returnStr.js
@@ -0,0 +1,7 @@
+// problem:
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// solution:
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
